21530aabd65f3a281c985817a544614f967a428c
alex198000 <85245556+alex198000@users.noreply.github.com>
2021-06-27 18:14:36 +0300

    button DZ

--- 3d ship Project/Assets/RW/Scripts/testingsScripts/TestMoove.cs
+++ 3d ship Project/Assets/RW/Scripts/testingsScripts/TestMoove.cs
@@ -25,8 +25,8 @@ public enum AffairsOfTheWeeks : int
 
 public enum Moovet : int
 {
-    Moove = 1,
-    Stop = 0
+    Stop,
+    Moove
 }
 
 
@@ -42,7 +42,7 @@ public class TestMoove : MonoBehaviour
     AffairsOfTheWeeks affairsOfTheWeeks;
     Moovet moovet;
 
-
+    public int result;
 
 
 
@@ -50,7 +50,7 @@ public class TestMoove : MonoBehaviour
 
     void Update()
     {
-        if (moovet != Moovet.Stop)
+        if (moovet == Moovet.Moove)
         {
             if (((transform.position.x >= -22f) && (direction == -1f)) || ((transform.position.x <= 22f) && (direction == 1f)))
 
@@ -84,8 +84,125 @@ public class TestMoove : MonoBehaviour
       
     }
 
+    public void Poned()
+    {
 
 
+        affairsOfTheWeeks = AffairsOfTheWeeks.Monday;// назначить сосотояние
+        int a = (int)affairsOfTheWeeks;// получить состояние
+        result = a;
+        //print(affairsOfTheWeeks);
+        //print("День недели " + a + " : Посетить зоопарк");
+
+    }
+
+    public void Vtorn()
+    {
+
+
+        affairsOfTheWeeks = AffairsOfTheWeeks.Tuesday;// назначить сосотояние
+        int a = (int)affairsOfTheWeeks;// получить состояние
+        result = a;
+        //print(affairsOfTheWeeks);
+        //print("День недели " + a + " : Посетить курсы");
+
+    }
+
+    public void Srerda()
+    {
+
+
+        affairsOfTheWeeks = AffairsOfTheWeeks.Wednesday;// назначить сосотояние
+        int a = (int)affairsOfTheWeeks;// получить состояние
+        result = a;
+        //print(affairsOfTheWeeks);
+        //print("День недели " + a + " : Посетить cпортзал");
+
+    }
+
+    public void Chetverg()
+    {
+
+
+        affairsOfTheWeeks = AffairsOfTheWeeks.Thursday;// назначить сосотояние
+        int a = (int)affairsOfTheWeeks;// получить состояние
+        result = a;
+        //print(affairsOfTheWeeks);
+        //print("День недели " + a + " : Посетить бассейн");
+
+    }
+
+    public void Pyatniza()
+    {
+
+
+        affairsOfTheWeeks = AffairsOfTheWeeks.Friday;// назначить сосотояние
+        int a = (int)affairsOfTheWeeks;// получить состояние
+        result = a;
+        //print(affairsOfTheWeeks);
+        //print("День недели " + a + " : Посетить курсы Unity");
+
+    }
+    public void Subbota()
+    {
+
+
+        affairsOfTheWeeks = AffairsOfTheWeeks.Saturday;// назначить сосотояние
+        int a = (int)affairsOfTheWeeks;// получить состояние
+        result = a;
+        //print(affairsOfTheWeeks);
+        //print("День недели " + a + " : Выходной1");
+
+    }
+    public void Voskr()
+    {
+
+
+        affairsOfTheWeeks = AffairsOfTheWeeks.Sunday;// назначить сосотояние
+        int a = (int)affairsOfTheWeeks;// получить состояние
+        result = a;
+        //print(affairsOfTheWeeks);
+        //print("День недели " + a + " : Выходной2");
+
+    }
+    public void Chekk()
+    {
+
+
+        if(result == 1)
+        {
+            print("Посетить зоопарк");
+        }
+        else if (result == 2)
+        {
+            print("Посетить курсы");
+        }
+        else if (result == 3)
+        {
+            print("Посетить cпортзал");
+        }
+        else if (result == 4)
+        {
+            print("Посетить бассейн");
+        }
+        else if (result == 5)
+        {
+            print("Посетить курсы Unity");
+        }
+        else if (result == 6)
+        {
+            print("Посетить кинотеатр");
+        }
+        else if (result == 7)
+        {
+            print("Выходной");
+
+        }
+        else 
+        {
+            print("Посетить зоопарк");
+        }
+    }
 
 
 }
